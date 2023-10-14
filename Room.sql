98e7d588ddc88746102b3f70fa1c85cba478fb20
ken7184 <kenkim7184@gmail.com>
2023-10-14 15:36:48 -0700

    Change room.sql

--- Room.sql
+++ Room.sql
@@ -2,15 +2,15 @@
 -- Query: SELECT * FROM Project.Room
 LIMIT 0, 200
 
--- Date: 2023-10-12 23:50
+-- Date: 2023-10-14 15:35
 */
-INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`) VALUES ('ParkSide','SanJose','2001',1000,'Yes');
-INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`) VALUES ('ParkSide','SanJose','2004',1000,'Yes');
-INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`) VALUES ('ParkSide','SanJose','2005',1000,'Yes');
-INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`) VALUES ('ParkSide','SanJose','2006',1200,'Yes');
-INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`) VALUES ('Parkside','SanJose','2007',1200,'Yes');
-INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`) VALUES ('ParkSide','SanJose','2008',1200,'Yes');
-INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`) VALUES ('ParkSide','SanJose','2009',1200,'Yes');
-INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`) VALUES ('ParkSide','SanJose','2010',1200,'Yes');
-INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`) VALUES ('ParkSide','SanJose','703',400,'No');
-INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`) VALUES ('ParkSide','SanJose','805',500,'No');
+INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`,`TypeName`,`Beds`,`hasBreakfast`) VALUES ('ParkSide','SanJose','2001',1000,'Yes','Large','2','Yes');
+INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`,`TypeName`,`Beds`,`hasBreakfast`) VALUES ('ParkSide','SanJose','2004',1000,'Yes','Large','2','Yes');
+INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`,`TypeName`,`Beds`,`hasBreakfast`) VALUES ('ParkSide','SanJose','2005',1000,'Yes','Large','2','Yes');
+INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`,`TypeName`,`Beds`,`hasBreakfast`) VALUES ('ParkSide','SanJose','2006',1200,'Yes','Large','2','Yes');
+INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`,`TypeName`,`Beds`,`hasBreakfast`) VALUES ('Parkside','SanJose','2007',1200,'Yes','Large','1','Yes');
+INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`,`TypeName`,`Beds`,`hasBreakfast`) VALUES ('ParkSide','SanJose','2008',1200,'Yes','Large','1','Yes');
+INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`,`TypeName`,`Beds`,`hasBreakfast`) VALUES ('ParkSide','SanJose','2009',1200,'Yes','Large','1','Yes');
+INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`,`TypeName`,`Beds`,`hasBreakfast`) VALUES ('ParkSide','SanJose','2010',1200,'Yes','Large','2','Yes');
+INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`,`TypeName`,`Beds`,`hasBreakfast`) VALUES ('ParkSide','SanJose','703',400,'No','Small','1','No');
+INSERT INTO `` (`HotelName`,`HotelLocation`,`RoomNumber`,`Price`,`Availability`,`TypeName`,`Beds`,`hasBreakfast`) VALUES ('ParkSide','SanJose','805',500,'No','Small','1','No');
